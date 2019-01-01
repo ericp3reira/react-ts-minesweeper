7e303a905f81579859a989d4f6fdd74c04ae1d98
Eric Pereira <ericmp@gmail.com>
2019-01-01 19:49:14 -0200

    Design MineSquare

---
+++ src/components/MineSquare.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export const MineSquare = (props: MineProps) => {
+  const { field, index, onLeftClick } = props;
+  return (
+    <button
+      className={`mine-button ${!field.isOpened ? 'mine-opened' : null}`}
+      tabIndex={index}
+      onClick={() => onLeftClick(field)}
+    >
+      { renderField(field) }
+    </button>
+  );
+};
+
+const renderField = (field: Mine) => {
+  if (field.isOpened && field.bombs > 0) {
+    return <span className={`bombs-${field.bombs}`}>{field.bombs}</span>;
+  }
+  if (field.isOpened && field.bombs == 0) {
+    return '';
+  } 
+  if (field.isOpened && field.bombs < 0) {
+    return <i className="fas fa-xs fa-bomb bomb" />;
+  }
+  if (!field.isOpened && field.isFlagged) {
+    return <i className="fas fa-xs fa-flag" />;
+  }
+
+  return '';
+};
+
+export interface MineProps {
+  index: number;
+  field: Mine;
+  onLeftClick: (field: Mine) => void;
+};
