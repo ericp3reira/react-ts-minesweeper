0a51f948e439207fe2232827642d80049ca3ea89
Eric Pereira <ericmp@gmail.com>
2019-01-01 20:23:36 -0200

    Import classes in components

--- src/components/MineField.tsx
+++ src/components/MineField.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Game, Mine } from '../domain';
 import MineSquare from './MineSquare';
 
 export const MineField = (props: MineFieldProps) => {
--- src/components/MineSquare.tsx
+++ src/components/MineSquare.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Mine } from '../domain';
 
 export const MineSquare = (props: MineProps) => {
   const { field, index, onLeftClick } = props;
