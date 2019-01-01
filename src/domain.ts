4432df7e25f201e9afeaf4842bb577dcc774ad37
Eric Pereira <ericmp@gmail.com>
2019-01-01 20:23:11 -0200

    Create Mine and Game classes

---
+++ src/domain.ts
@@ -0,0 +1,21 @@
+export interface Point {
+  x: number;
+  y: number;
+};
+
+export class Mine {
+  constructor(
+    public position: Point,
+    public isOpened = false,
+    public bombs = 0,
+    public isFlagged = false
+  ) {}
+};
+
+export class Game {
+  constructor(
+    public state: Array<Array<Mine>>>,
+    public totalBombs = 0,
+    public exploded = false
+  ) {}
+};
