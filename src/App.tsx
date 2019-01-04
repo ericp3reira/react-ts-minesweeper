ffe773807359ac538ce3e7ec0ffdcd87ddada447
Eric Pereira <ericmp@gmail.com>
2019-01-04 17:11:45 -0200

    Refactor App skeleton and add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { newGame } from './newGame';
 
 class App extends Component {
   render() {
-    const game = newGame(5, 5);
+    const grid = newGame(16, 16);
     const onLeftClick = (field: Mine) => {
       console.log(field);
     }
@@ -17,8 +17,25 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Timer elapsedSeconds={seconds} />
-        <MineField game={game} onLeftClick={onLeftClick} />
+        <header className="App-header">
+          <div className="display">
+            <span className="icon">💣</span>
+            <span className="counter">8</span>
+          </div>
+          <div className="time-display">
+            <Timer elapsedSeconds={seconds} />
+          </div>
+          <div className="display">
+            <span className="counter">8</span>
+            <span className="icon">🚩</span>
+          </div>
+        </header>
+        <main className="App-grid">
+          <MineField game={grid} onLeftClick={onLeftClick} />
+        </main>
+        <footer>
+          Built with <span className="icon">☕️</span> by @ericp3reira
+        </footer>
       </div>
     );
   }
